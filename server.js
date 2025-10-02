6529418e32dcf17abfef046afbd84269915359c3
ahmed <ahmedalzoghrory@gmail.com>
2025-10-02 21:40:10 +0300

    feat: Implement asynchronous HTTP server to serve HTML

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port=3000;
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
-   
-    const filePath = path.join(__dirname, 'index.html');
-    
- 
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('خطأ في تحميل الصفحة');
-            return;
-        }
-        
-        
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(data);
-    });
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
 });
 
-
-server.listen(3000, () => {
-    console.log('الخادم يعمل على http://localhost:' + port);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
